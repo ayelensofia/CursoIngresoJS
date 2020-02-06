4d447aab3c1f649df2caed3502ac86bcc4011a45
Aguirre Ayelen <60743773+ayelensofia@users.noreply.github.com>
2020-02-06 18:17:59 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,22 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
 function mostrar()
 {
-	alert("ok");
-}
+
+	var apellido;//definimos variable
+	var nombre;
+
+	apellido="perez";//asignamos valor a la variable
+	nombre="juan";
+
+	alert(nombre);//variable
+	alert(apellido);//variable
+
+
+	//es un valor literal si esta entre ""
+	alert("tal cual");//literal
+	alert("apellido");//literal
+	alert(apellido);//variable 
+	alert(primer pri)//si la variable no existe no se puede hacer nada
+	alert();//sale vacio
+}	
 
