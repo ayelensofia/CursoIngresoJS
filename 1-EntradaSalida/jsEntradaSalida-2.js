ea2631c128f46297c04646a800b895bd1b5eb7bb
Aguirre Ayelen <60743773+ayelensofia@users.noreply.github.com>
2020-02-07 19:34:00 -0300

    Update jsEntradaSalida-2.js

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,7 +2,7 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombre = prompt("ingrese su nombre"); //defino variable
+	var nombre = prompt("ingrese su nombre"); //devuelve el dato
 	alert(nombre)//muestra el dato almacenado en la variable
 	
 	console.log("su nombre es"	+	nombre);//nos permite ver desde la consola
