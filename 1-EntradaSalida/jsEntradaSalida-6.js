0b033f18b8e1fce020f6b42102e4d9f42c13b9e8
Aguirre Ayelen <60743773+ayelensofia@users.noreply.github.com>
2020-02-07 20:25:40 -0300

    ejercicio 6
    parseInt+alert

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -4,13 +4,19 @@ mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
 	var primernumero;
-	var	segundonumero;
+	var segundonumero;
 	var suma;
 
-	primernumero=document.getElementById('numeroUno').value;
+	var primernumero=document.getElementById('numeroUno').value;
 	primernumero=parseInt(primernumero);
-	segundonumero=document.getElementById('numeroDos').value;
+	var segundonumero=document.getElementById('numeroDos').value;
 	segundonumero=parseInt(segundonumero);
+
 	suma=primernumero+segundonumero;
 
+	alert("la suma es"+suma);
+
+
+	
 }
+
