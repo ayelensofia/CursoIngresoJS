58b47954dec6e4ac3be8a53bfd816b110f7786c7
Aguirre Ayelen <60743773+ayelensofia@users.noreply.github.com>
2020-02-07 20:31:39 -0300

    ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,37 +3,70 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	//definicion de variables
+{	
 	var primernumero;
 	var segundonumero;
-	var resultadoDeLaSuma;
-	var resultadoDeLaresta;
-	//sino utilizo como palabra lo transformo en numero
-	primernumero=document.getElementById('numeroUno').value;
-	primernumero=parseInt(primernumero);
+	var suma;
 
-	segundonumero=document.getElementById('numeroDos').value;
+	var primernumero=document.getElementById('numeroUno').value;
+	primernumero=parseInt(primernumero);
+	var segundonumero=document.getElementById('numeroDos').value;
 	segundonumero=parseInt(segundonumero);
 
-	resultadoDeLaSuma=primernumero+segundonumero;
-	resultadoDeLaresta=primernumero-segundonumero;
+	suma=primernumero+segundonumero;
 
-	console.log(resultadoDeLaSuma);
+	alert("la suma es"+suma);
 
 }
 
 function restar()
 {
-	
+	var primernumero;
+	var segundonumero;
+	var resta;
+
+	var primernumero=document.getElementById('numeroUno').value;
+	primernumero=parseInt(primernumero);
+	var segundonumero=document.getElementById('numeroDos').value;
+	segundonumero=parseInt(segundonumero);
+
+	resta=primernumero-segundonumero;
+
+	alert("la resta es"+resta);
+
 }
 
 function multiplicar()
 { 
-	
+	var primernumero;
+	var segundonumero;
+	var multiplicacion;
+
+	var primernumero=document.getElementById('numeroUno').value;
+	primernumero=parseInt(primernumero);
+	var segundonumero=document.getElementById('numeroDos').value;
+	segundonumero=parseInt(segundonumero);
+
+	multiplicacion=primernumero*segundonumero;
+
+	alert("la multiplicacion es"+multiplicacion);
+
 }
 
 function dividir()
 {
-	
+	var primernumero;
+	var segundonumero;
+	var division;
+
+	var primernumero=document.getElementById('numeroUno').value;
+	primernumero=parseInt(primernumero);
+	var segundonumero=document.getElementById('numeroDos').value;
+	segundonumero=parseInt(segundonumero);
+
+	division=primernumero/segundonumero;
+
+	alert("la division es"+division);
+
 }
 
