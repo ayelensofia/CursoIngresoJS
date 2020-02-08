51ea6aee5b8c391dfb417d885de39b0884fc4a93
Aguirre Ayelen <60743773+ayelensofia@users.noreply.github.com>
2020-02-08 13:01:46 -0300

    facturacion tp

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,62 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var preciouno;
+	var preciodos;
+	var preciotres;
+	var suma;
+
+	preciouno=document.getElementById('PrecioUno').value;
+	preciouno=parseInt(preciouno);
+	preciodos=document.getElementById('PrecioDos').value;
+	preciodos=parseInt(preciodos);
+	preciotres=document.getElementById('PrecioTres').value;
+	preciotres=parseInt(preciotres);
+
+	suma=preciouno+preciodos+preciotres;
+	alert(suma);
 }
 function Promedio () 
 {
+	var preciouno;
+	var preciodos;
+	var preciotres;
+	var promedio;
+	var suma;
+
+	preciouno=document.getElementById('PrecioUno').value;
+	preciouno=parseInt(preciouno);
+	preciodos=document.getElementById('PrecioDos').value;
+	preciodos=parseInt(preciodos);
+	preciotres=document.getElementById('PrecioTres').value;
+	preciotres=parseInt(preciotres);
+	suma=preciouno+preciodos+preciotres;
+	promedio=suma/3;
+	alert(promedio);
 	
+
+
 }
 function PrecioFinal () 
 {
+	var preciouno;
+	var preciodos;
+	var preciotres;
+	var suma;
+	var preciofinal;
+	var porcentaje;
+	var aumento;
+
+	porcentaje=21
+	preciouno=document.getElementById('PrecioUno').value;
+	preciouno=parseInt(preciouno);
+	preciodos=document.getElementById('PrecioDos').value;
+	preciodos=parseInt(preciodos);
+	preciotres=document.getElementById('PrecioTres').value;
+	preciotres=parseInt(preciotres);
+	suma=preciouno+preciodos+preciotres;
+	aumento=suma*porcentaje/100;
+	preciofinal=suma+aumento;
+	alert(preciofinal);
 	
 }
