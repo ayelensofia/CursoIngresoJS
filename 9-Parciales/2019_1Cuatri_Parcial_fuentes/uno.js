dc29b7e6b96aa0e638d8dd8e98604326a2458616
Aguirre Ayelen <60743773+ayelensofia@users.noreply.github.com>
2020-02-12 21:48:18 -0300

    parcial-ejercicio1

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,19 @@
 
 function mostrar()
 {
+	var ladouno;
+	var	ladodos;
+	var ladotres;
+	var perimetro;
 
+	ladouno=prompt("ingrese el primer lado");
+	ladouno=parseInt(ladouno);
+	ladodos=prompt("ingrese el segundo lado");
+	ladodos=parseInt(ladodos);
+	ladotres=prompt("ingrese el tercer lado");
+	ladotres=parseInt(ladotres);
+
+	perimetro=ladouno*3;
+
+		alert(perimetro);
 }
